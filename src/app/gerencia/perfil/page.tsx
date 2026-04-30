4ef5f40a9e6c772f7799d417cd518048297e4e24
Angel <tu-email@ejemplo.com>
2026-04-30 10:40:32 -0300

    Fix remaining occurrences of dueno

--- src/app/gerencia/perfil/page.tsx
+++ src/app/gerencia/perfil/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { UnderConstruction } from "@/components/shared/UnderConstruction";
 
-export default function PerfildeDueoPage() {
+export default function PerfilGerenciaPage() {
   return (
     <div className="container mx-auto p-4 md:p-8">
-      <UnderConstruction moduleName="DUEÑO" pageName="Perfil de Gerencia" />
+      <UnderConstruction moduleName="GERENCIA" pageName="Perfil de Gerencia" />
     </div>
   );
 }
